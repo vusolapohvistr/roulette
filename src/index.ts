6fe9d737a407534cc09d5c32bcb7ae574e33bfbb
Serhii Pylypchuk <sergiydjan@gmail.com>
2020-05-26 10:46:08 +0300

    iOS safari fix #1

--- src/index.ts
+++ src/index.ts
@@ -151,8 +151,10 @@ window.notLegacyRoulette = (() => {
                 roulette.style.setProperty('transition', `all ${lastStageDuration}s ${cubic} 0s`);
                 roulette.style.setProperty('transform', `translate3d(0px, 0px, 0px)`);
 
+                const centre = Math.min(window.innerWidth / 2, screen.width / 2)
+
                 const winnerPosition = (roulette.children[winnerNodeIndex] as HTMLDivElement).getBoundingClientRect();
-                const lastOffset = winnerPosition.left - window.innerWidth / 2 + itemWidth / 2;
+                const lastOffset = winnerPosition.left - centre + itemWidth / 2;
                 mainAnimation.onfinish = () => {
                     !finishedStart && setTimeout(() => {
                         roulette.style.setProperty('transform', `translate3d(-${lastOffset}px, 0px, 0px)`);
